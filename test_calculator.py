d2f3f94df051d839f1c847704671d432ede61e5c
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-07-14 21:54:55 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -15,7 +15,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtraction(self):
         result = calculator.subtract(4,5)
-        self.assertEqual(result, 3)
+        self.assertEqual(result, 1)
         
         
 if __name__ == '__main__':
