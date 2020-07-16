70bb76c3d7e497903e381f101bebef79e3bf1381
Abhishek Kumar (PES) <abhishekk40@kpit.com>
2020-07-16 11:17:14 +0530

    Test Webhook TRy 2

--- test_calculator.py
+++ test_calculator.py
@@ -9,8 +9,8 @@ import unittest
 class TestCalculator(unittest.TestCase):
 
     def test_addition(self):
-        result = calculator.add(2, 4)
-        self.assertEqual(result, 6)
+        result = calculator.add(2, 5)
+        self.assertEqual(result, 7)
         
 
     def test_subtraction(self):
