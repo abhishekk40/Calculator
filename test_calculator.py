9eff35228ceceac2fb2ee8146413c07bda108bbd
Abhishek Kumar (PES) <abhishekk40@kpit.com>
2020-09-29 13:28:21 +0530

    test

--- test_calculator.py
+++ test_calculator.py
@@ -9,8 +9,8 @@ import unittest
 class TestCalculator(unittest.TestCase):
 
     def test_addition(self):
-        result = calculator.add(2, 6)
-        self.assertEqual(result, 8)
+        result = calculator.add(2, 7)
+        self.assertEqual(result, :9)
         
 
     def test_subtraction(self):
